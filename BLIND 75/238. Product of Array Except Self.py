1d3666f393b8af6526ee64a7833758719e7bee51
Sri Krishna Nijanthan <92555623+ssknijanthan@users.noreply.github.com>
2024-03-16 21:06:39 +0530

    Update and rename 238. Product of Array Except Self to 238. Product of Array Except Self.py

--- BLIND 75/238. Product of Array Except Self.py
+++ BLIND 75/238. Product of Array Except Self.py
@@ -2,14 +2,19 @@ class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n=len(nums)
         result=[1]*n
-        #calculating the left prefix product
+        # calculating the left prefix product
         left_product=1
         for i in range(n):
             result[i]=result[i]*left_product
             left_product=left_product*nums[i]
-        # calculating the right prefix and multiplying it with the existing     left_prefix in result array
         right_product=1
         for i in range(n-1,-1,-1):
             result[i]=result[i]*right_product
             right_product=right_product*nums[i]
         return result
+
+"""
+Intuition: calculating the right prefix and multiplying it with the existing left_prefix in result array.
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
